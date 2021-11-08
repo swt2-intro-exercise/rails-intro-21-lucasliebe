ae5459eb3f89f2b46d2055a1e727f362b5e0cf94
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-08 21:44:15 +0100

    add Author Model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it "create and check Author" do
+	author = Author.new()
+	author.first_name = "Alan"
+	author.last_name = "Turing"
+	author.homepage = "https://wikipedia.org/Alan_Turing"
+        expect(author.first_name).to eq("Alan")
+        expect(author.last_name).to eq("Turing")
+        expect(author.homepage).to eq("https://wikipedia.org/Alan_Turing")
+  end
+end
