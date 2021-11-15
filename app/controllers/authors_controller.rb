5ba6ac748ac9d0164d9ee365a1bd0e8841875325
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-15 20:48:54 +0100

    add edit functionality

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,12 +8,22 @@ class AuthorsController < ApplicationController
 	def new
 	  @author = Author.new
 	end
+	def edit
+	  @author = Author.find(params[:id])
+	end
 	def create
 	  @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
 	  if @author.save
-	    redirect_to root_path, notice: 'Success!'
+	    redirect_to @author
 	  else
 	    render 'new'
 	  end
 	end
+	def update
+	  @author = Author.find(params[:id])
+	  if @author.update(params.require(:author).permit(:first_name, :last_name, :homepage))
+	    redirect_to @author
+	  else
+	    render 'edit'
+	  end
 end
