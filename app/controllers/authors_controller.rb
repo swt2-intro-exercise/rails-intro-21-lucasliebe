89d40ee165451b9ea24c297b39f276ea75cb5d4b
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-15 20:56:28 +0100

    add delete functionality

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -26,5 +26,10 @@ class AuthorsController < ApplicationController
 	  else
 	    render 'edit'
 	  end
+	end	
+	def destroy
+	  @author = Author.find(params[:id])
+	  @author.destroy 
+	  redirect_to authors_path
 	end
 end
