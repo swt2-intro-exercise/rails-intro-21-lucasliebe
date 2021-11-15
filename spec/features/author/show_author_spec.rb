5413aee86a1bc2c3856f37b941e05a3e125704a8
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-15 19:32:55 +0100

    fix local test

--- spec/features/author/show_author_spec.rb
+++ spec/features/author/show_author_spec.rb
@@ -4,6 +4,6 @@ describe "Show author page", type: :feature do
   it "visit Alan Details page" do
         @alan = create :author
 	visit author_path(@alan)
-	expect(page).to have_text("@alan.first_name")
+	expect(page).to have_text(@alan.first_name)
   end
 end
