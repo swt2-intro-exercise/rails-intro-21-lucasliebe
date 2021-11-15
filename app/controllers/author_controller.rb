c8d9c95da7501369bfe6de53de1a211c163f8fd3
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-15 11:20:31 +0100

    save authors

--- app/controllers/author_controller.rb
+++ app/controllers/author_controller.rb
@@ -1,4 +1,14 @@
 class AuthorController < ApplicationController
+	def index
+	end
+	def show
+    	  @author = Author.find(params[:id])
+  	end
 	def new
+	end
+	def create
+	  @author = Author.new(params.require(:author).permit(:first_name, :last_name, :homepage))
+	  @author.save
+	  redirect_to root_path, notice: 'Success!'
 	end
 end
