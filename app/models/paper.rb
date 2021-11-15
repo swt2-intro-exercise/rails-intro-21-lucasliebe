5ef829277204732c2c7a77b330ebe8709de55c7a
Lucas Liebe <lucasliebe.ll@gmail.com>
2021-11-15 21:53:26 +0100

    add has_and_belongs_to_many attribute

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ApplicationRecord
-	has_and_belongs_to_many :author
+	has_and_belongs_to_many :authors
 	validates :title, presence: true
 	validates :venue, presence: true
 	validates :year, presence: true, numericality: { only_integer: true }
